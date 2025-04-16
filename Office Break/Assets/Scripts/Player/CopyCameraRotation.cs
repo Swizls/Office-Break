e2f006878a7a7879ffa1ee4c580a6723ef87c822
Rihards Fabriks <swizls12@gmail.com>
2025-04-16 19:54:37 +0300

    IK следование туловища за камерой

--- Office Break/Assets/Scripts/Player/CopyCameraRotation.cs
+++ Office Break/Assets/Scripts/Player/CopyCameraRotation.cs
@@ -4,7 +4,7 @@ public class CopyCameraRotation : MonoBehaviour
 {
     [SerializeField] private Transform _cameraTransform;
 
-    private void Update()
+    private void LateUpdate()
     {
         transform.rotation = Quaternion.Euler(transform.eulerAngles.x, _cameraTransform.eulerAngles.y, transform.eulerAngles.z);        
     }
