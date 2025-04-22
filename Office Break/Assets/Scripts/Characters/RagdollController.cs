dfaf72cae2e6e156ec4683572f213ffb8dfaeb11
Rihards Fabriks <swizls12@gmail.com>
2025-04-22 08:47:49 +0300

    Рефакторинг логики RagdollHandler

--- Office Break/Assets/Scripts/Characters/RagdollController.cs
+++ Office Break/Assets/Scripts/Characters/RagdollController.cs
@@ -1,30 +1,22 @@
-using OfficeBreak.Characters.FightingSystem;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace OfficeBreak.Characters
 {
-    [RequireComponent(typeof(Animator))]
-    public abstract partial class RagdollController : MonoBehaviour
+    public class RagdollController : MonoBehaviour
     {
         [SerializeField] protected GameObject _rootBone;
-        [SerializeField] protected CharacterController _characterController;
-
-        private Animator _animator;
-        private AttackController _attackController;
 
         private Collider[] _colliders;
         private Rigidbody[] _rigidbodies;
 
-        private void Awake() => Initialize();
+        public UnityEvent OnRagdollEnable;
+        public UnityEvent OnRagdollDisable;
 
-        protected virtual void Initialize()
+        private void Awake()
         {
-            _animator = GetComponent<Animator>();
-
             _colliders = _rootBone.GetComponentsInChildren<Collider>();
             _rigidbodies = _rootBone.GetComponentsInChildren<Rigidbody>();
-
-            _attackController = GetComponentInParent<AttackController>();
 
             DisableRagdoll();
         }
@@ -32,35 +24,23 @@ namespace OfficeBreak.Characters
         public virtual void EnableRagdoll()
         {
             foreach (Collider collider in _colliders)
-            {
                 collider.enabled = true;
-            }
 
             foreach (Rigidbody rigidbody in _rigidbodies)
-            {
                 rigidbody.isKinematic = false;
-            }
 
-            _animator.enabled = false;
-            _characterController.enabled = false;
-            _attackController.enabled = false;
+            OnRagdollEnable?.Invoke();
         }
 
         public virtual void DisableRagdoll()
         {
             foreach (Collider collider in _colliders)
-            {
                 collider.enabled = false;
-            }
 
             foreach (Rigidbody rigidbody in _rigidbodies)
-            {
                 rigidbody.isKinematic = true;
-            }
 
-            _animator.enabled = true;
-            _characterController.enabled = true;
-            _attackController.enabled = true;
+            OnRagdollDisable?.Invoke();
         }
     }
 }
