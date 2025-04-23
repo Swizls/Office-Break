0493dc08ba9211fa0938f1294ec3320c884ddc3d
Rihards Fabriks <swizls12@gmail.com>
2025-04-23 11:04:11 +0300

    Состояние атаки для врага

--- Office Break/Assets/Scripts/Characters/Enemies/AI/Behaviours/EnemyAttackBehaviour.cs
+++ Office Break/Assets/Scripts/Characters/Enemies/AI/Behaviours/EnemyAttackBehaviour.cs
@@ -7,6 +7,8 @@ namespace OfficeBreak.Characters.Enemies.AI
     {
         private EnemyAttackController _attackController;
 
+        private bool IsEnoughCloseToAttack => Vector3.Distance(PlayerTransform.position, EnemyTransform.position) < _attackController.AttackRange;
+
         public EnemyAttackBehaviour(Transform playerTransform, EnemyMover mover, EnemyAttackController attackController) : base(playerTransform, mover)
         {
             _attackController = attackController;
@@ -15,6 +17,9 @@ namespace OfficeBreak.Characters.Enemies.AI
         public override void Execute()
         {
             Mover.SetDestination(PlayerTransform.position);
+
+            if (IsEnoughCloseToAttack)
+                _attackController.PerformAttack();
         }
     }
 }
--- Office Break/Assets/Scripts/Characters/Enemies/AI/Behaviours/EnemyBehaviour.cs
+++ Office Break/Assets/Scripts/Characters/Enemies/AI/Behaviours/EnemyBehaviour.cs
@@ -5,11 +5,13 @@ namespace OfficeBreak.Characters.Enemies.AI
     public abstract class EnemyBehaviour
     {
         protected Transform PlayerTransform { get; private set; }
+        protected Transform EnemyTransform { get; private set; }
         protected EnemyMover Mover { get; private set; }
 
         public EnemyBehaviour(Transform playerTransform, EnemyMover mover)
         {
             PlayerTransform = playerTransform;
+            EnemyTransform = mover.transform;
             Mover = mover;
         }
 
