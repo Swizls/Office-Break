6d27c6436074aed66215c879b9958fe46e56fa0f
Rihards Fabriks <swizls12@gmail.com>
2025-04-24 09:45:34 +0300

    Добавлен звук лифта

--- Office Break/Assets/Scripts/ElevatorDoorsController.cs
+++ Office Break/Assets/Scripts/ElevatorDoorsController.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 namespace OfficeBreak
 {
     [RequireComponent (typeof(Animator))]
+    [RequireComponent(typeof(AudioSource))]
     public class ElevatorDoorsController : MonoBehaviour
     {
         private const string IS_OPEN = "isOpen";
         private const float OPEN_TIMER = 5f;
 
         private Animator _animator;
+        private AudioSource _audioSource;
 
-        private void Awake() => _animator = GetComponent<Animator>();
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _audioSource = GetComponent<AudioSource>();
+        }
 
         private IEnumerator KeepDoorsOpen()
         {
@@ -23,6 +29,7 @@ namespace OfficeBreak
         {
             _animator.SetBool(IS_OPEN, true);
             StartCoroutine(KeepDoorsOpen());
+            _audioSource.Play();
         }
 
         public void CloseDoors() => _animator.SetBool(IS_OPEN, false);
