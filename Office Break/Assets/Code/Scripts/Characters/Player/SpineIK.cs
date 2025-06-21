ef23a1b32989619281d59b6b698eee868f593431
Rihards Fabriks <swizls12@gmail.com>
2025-06-22 00:15:18 +0300

    Заменена моделька игрока

--- Office Break/Assets/Code/Scripts/Characters/Player/SpineIK.cs
+++ Office Break/Assets/Code/Scripts/Characters/Player/SpineIK.cs
@@ -4,12 +4,39 @@ namespace OfficeBreak
 {
     public class SpineIK : MonoBehaviour
     {
+        private enum Direction { X, Y, Z }
+
+        [SerializeField] private Direction _direction;
         [SerializeField] private Transform _targetTransform;
+
+        [SerializeField] private float _xOffset;
         [SerializeField] private float _yOffset;
+        [SerializeField] private float _zOffset;
+
+        [SerializeField] private bool _useInvertedValue = false;
 
         private void LateUpdate()
         {
-            transform.rotation = Quaternion.Euler(_targetTransform.localEulerAngles.x, _targetTransform.eulerAngles.y + _yOffset, _targetTransform.localEulerAngles.z);
+            float primaryDirectionAngle = _useInvertedValue ? -_targetTransform.eulerAngles.x : _targetTransform.eulerAngles.x;
+
+            float x = transform.eulerAngles.x;
+            float y = transform.eulerAngles.y;
+            float z = transform.eulerAngles.z;
+
+            switch (_direction)
+            {
+                case Direction.Y:
+                    y = primaryDirectionAngle;
+                    break;
+                case Direction.Z:
+                    z = primaryDirectionAngle; 
+                    break;
+                default:
+                    x = primaryDirectionAngle;
+                    break;
+            }
+
+            transform.rotation = Quaternion.Euler(x + _xOffset, y + _yOffset, z + _zOffset);
         }
     }
 }
